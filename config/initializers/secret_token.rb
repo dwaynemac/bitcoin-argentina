4860694feb26d890d33b5846f79fa38a7e34ec00
Dwayne Macgowan <dwaynemac@gmail.com>
2011-07-03 16:11:14 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitcoinArgentina::Application.config.secret_token = '09d42a6b6cea1fdf12d3c61994d4fddc88e8506361c55675b710ada8cb7914e9089eba8fae382a21a415b1e22030c7beb972f739c3c105ea47eca7a04a9360a0'
