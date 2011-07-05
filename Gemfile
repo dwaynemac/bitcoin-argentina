44d00abb10988a0f849b62e9e523eb67c954e289
Dwayne Macgowan <dwaynemac@gmail.com>
2011-07-05 14:09:33 -0300

    install blog, add btc-ars rate

--- Gemfile
+++ Gemfile
@@ -45,12 +45,12 @@ end
 # END REFINERY CMS ============================================================
 
 # USER DEFINED
-
+gem 'typhoeus'
 
 # Specify additional Refinery CMS Engines here (all optional):
 # gem 'refinerycms-inquiries',    '~> 1.0'
 # gem "refinerycms-news",         '~> 1.2'
-# gem 'refinerycms-blog',         '~> 1.6'
+gem 'refinerycms-blog',         '~> 1.6'
 # gem 'refinerycms-page-images',  '~> 1.0'
 
 # Add i18n support (optional, you can remove this if you really want to).
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  before_filter :get_bitcoin_rate
+
+  private
+  def get_bitcoin_rate
+    @btc_usd = "puto"
+    response = Typhoeus::Request.get("http://bitcoincharts.com/t/weighted_prices.json")
+    @btc_usd = ActiveSupport::JSON.decode(response.body)["USD"]["24h"]
+    response = Typhoeus::Request::get("http://www.google.com/ig/calculator?hl=en&q=#{@btc_usd}USD%3D%3FARS")
+    @btc_ars = ActiveSupport::JSON.decode(response.body)["rhs"].gsub(/Argentine Pesos/,"").to_f
+  end
 end
